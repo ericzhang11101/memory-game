b0de31f83274a0d06617e94120e632f20f5d183f
ericzhang11101 <ericzhang11101@gmail.com>
2021-09-13 17:08:31 -0400

    removed test code

--- src/App.js
+++ src/App.js
@@ -238,7 +238,6 @@ function App() {
         score={score}
         highScore={highScore}
       />
-      <h1>test</h1>
       <div className="card-container">
         {
           cards.map((card) => {
